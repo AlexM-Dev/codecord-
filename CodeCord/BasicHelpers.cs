3112ee49bef978e64a78a8ee684823d8ab60d0c6
AlexM-Dev <mac0017@drouinsc.vic.edu.au>
2018-04-09 11:15:27 +1000

    Basic Helpers

---
+++ CodeCord/BasicHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeCord {
+    class BasicHelpers {
+        public static string RemoveLines(string str) {
+            return Regex.Replace(str, "[\r\n(\r\n)]", "");
+        }
+    }
+}
