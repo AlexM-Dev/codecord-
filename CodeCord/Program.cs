7b342377c7f2f9e6070435bf2b020b08350e5d65
AlexM-Dev <mac0017@drouinsc.vic.edu.au>
2018-04-09 12:24:53 +1000

    Variables?

--- CodeCord/Program.cs
+++ CodeCord/Program.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodeCord
-{
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            string lines = "\tHello\r\n    Noob!";
-            Console.WriteLine(lines);
-            Console.WriteLine(BasicHelpers.Normalise(lines));
+namespace CodeCord {
+    internal class Program {
+        private static void Main(string[] args) {
+            var manager = new VariableManager();
+            manager.Variables.Add("v", 10);
+
+            object v = manager.Variables["v"];
+            Console.WriteLine(v);
+
+            // Won't work.
+            manager.Set("v", 100d);
+            Console.WriteLine(manager.Variables["v"]);
+
+            // Will work.
+            manager.Set("v", 1000);
+            Console.WriteLine(manager.Variables["v"]);
+
             Console.ReadKey();
         }
     }
---
+++ CodeCord/VariableManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCord {
+    class VariableManager {
+        public Dictionary<string, object> Variables { get; set; } =
+            new Dictionary<string, object>();
+
+        public void Set(string name, object value) {
+            if (value.GetType() != Variables[name].GetType())
+                return;
+            Variables[name] = value;
+        }
+    }
+}
